8aaafbb5b2e8af80cfdacc763195bf4fe5f45781
Rifat Arefin <arefinrifat786@gmail.com>
2024-10-13 00:23:59 +0600

    Add JobSeeker Profile to the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\Education::factory()->create();
+        \App\Models\Experience::factory()->create();
+        \App\Models\Skill::factory()->create();
     }
 }
