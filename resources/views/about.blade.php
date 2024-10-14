2e32484e02818434d5569089f63b5f349dff1c48
Rifat Arefin <arefinrifat786@gmail.com>
2024-10-15 00:26:29 +0600

    Update everything

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
     <x-slot:pageName> About Us </x-slot:pageName> 
     <x-slot:title> About Us | Job Portal </x-slot:title>
-    <h1>Hello From About Page </h1> 
+    <x-page 
 </x-layout>
